ec2b9ede60b788f024c9bd5cf9664e7cf2d5ecdc
Abu Yousuf <yousuf.kuet08@gmail.com>
2016-08-02 18:26:27 +0600

    Initial commit

---
+++ app/src/main/java/com/shawon/yousuf/fileupload/util/Constants.java
@@ -0,0 +1,13 @@
+package com.shawon.yousuf.fileupload.util;
+
+/**
+ * Created by Yousuf on 8/2/2016.
+ */
+public class Constants {
+
+    // File upload url (replace the ip with your server address)
+    public static final String FILE_UPLOAD_URL = "http://192.168.0.104/AndroidFileUpload/fileUpload.php";
+
+    // Directory name to store captured images and videos
+    public static final String IMAGE_DIRECTORY_NAME = "File Upload Data";
+}
